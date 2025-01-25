980435ea7be2f6120942a23ae9bbd37fafd18178
69learn <144735591+69learn@users.noreply.github.com>
2025-01-25 23:00:23 +0330

    Update lightweight.sh

--- lightweight.sh
+++ lightweight.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 apt update -y
 apt install wget -y
-wget -O /etc/logo2.sh https://github.com/Azumi67/UDP2RAW_FEC/raw/main/logo2.sh
-chmod +x /etc/logo2.sh
+wget -O /etc/logo.sh https://github.com/69learn/CHISEL-AZUMI/blob/main/logo.sh
+chmod +x /etc/logo.sh
 if [ -f "light_script.py" ]; then
     rm light_script.py
 fi
