069b86c0b26e8ed8a3c2dd8026bc435599ce48c4
69learn <144735591+69learn@users.noreply.github.com>
2025-01-25 22:43:21 +0330

    Update ubuntu24.sh

--- ubuntu24.sh
+++ ubuntu24.sh
@@ -6,5 +6,5 @@ chmod +x /etc/logo2.sh
 if [ -f "ubuntu24.py" ]; then
     rm ubuntu24.py
 fi
-wget https://github.com/69learn/6to4-azumi/releases/download/ubuntu24/ubuntu24.py
+wget https://github.com/69learn/6to4-azumi/refs/heads/main/releases/download/ubuntu24/ubuntu24.py
 python3 ubuntu24.py
